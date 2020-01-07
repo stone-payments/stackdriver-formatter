233f7f83ef80d547e7a9d382ccebc3bf988903cd
Henrique Escobar Hornos <henrique.hornos_ext@stone.com.br>
2020-01-07 13:20:01 -0300

    update package version to Release 1.0.0

--- setup.py
+++ setup.py
@@ -14,5 +14,5 @@ setup(
     packages=find_packages(exclude=['tests']),
     py_modules=['stackdriver_formatter'],
     url='https://github.com/stone-payments/stackdriver-formatter',
-    version='0.1.0',
+    version='1.0.0',
 )
