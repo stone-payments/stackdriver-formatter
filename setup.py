4e47be602b505ccece28516ec26e6b6df23ce9a0
Henrique Escobar Hornos <henrique.hornos_ext@stone.com.br>
2020-01-08 10:00:03 -0300

    fix requirementes and setup file with packets versions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     description='Stackdriver Formatter',
-    install_requires=['python-json-logger'],
+    install_requires=['python-json-logger>=0.1.11'],
     long_description=open('README.md').read().strip(),
     name='stackdriver_formatter',
     packages=find_packages(exclude=['tests']),
